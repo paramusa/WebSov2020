4cb7a9eb36c39169750eb6e197737304a5356b69
paramusa <50746443+paramusa@users.noreply.github.com>
2020-11-04 17:56:38 +0200

    Lab 7 valmis

--- labs/07 JavaScript Language/js/tasks.js
+++ labs/07 JavaScript Language/js/tasks.js
@@ -2,7 +2,7 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
@@ -38,9 +38,33 @@ function addTask() {
     
 } // End of addTask() function.
 
+function removeDuplicates(){
+    'use strict';
+
+    let uniikit = [];
+    for (let i = 0; i < tasks.length; i++) {
+        if (!(uniikit.includes(tasks[i]))){
+            uniikit.push(tasks[i]);
+        } else tasks.splice(i, 1);
+    }
+    tasks = uniikit;
+
+    // Reference to where the output goes:
+    let output = document.getElementById('output');
+    let message = '';
+    // Update the page:
+    message = '<h2>To-Do</h2><ol>';
+    for (var i = 0, count = tasks.length; i < count; i++) {
+        message += '<li>' + tasks[i] + '</li>';
+    }
+    message += '</ol>';
+    output.innerHTML = message;
+}
+
 // Initial setup:
 function init() {
     'use strict';
     document.getElementById('theForm').onsubmit = addTask;
+    document.getElementById('removeduplicates').onclick = removeDuplicates;
 } // End of init() function.
 window.onload = init;
