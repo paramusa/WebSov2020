f31a3397d7c0d5c36bda60e62e54582e412dc896
paramusa <50746443+paramusa@users.noreply.github.com>
2020-12-17 15:39:26 +0200

    Move backend

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require('./router');
+
+const app = express();
+app.use(express.json());
+app.use('/api/', router);
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/reseptisivu', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../reseptisivu/dist'));
+}
+
+const server = app.listen(PORT, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
