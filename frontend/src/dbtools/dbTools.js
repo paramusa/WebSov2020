7a1d5b18ae434637d8c256b60a295839518532b6
paramusa <50746443+paramusa@users.noreply.github.com>
2020-12-17 21:34:23 +0200

    Heroku apiserver

--- frontend/src/dbtools/dbTools.js
+++ frontend/src/dbtools/dbTools.js
@@ -1,24 +1,26 @@
 const axios = require('axios');
 
+const apiserver = process.env.APISERVER;
+
 //get all
 async function getAllRecipes() {
-    const response = await axios.get('/api/recipes');
+    const response = await axios.get(apiserver + '/api/recipes');
     return response.data;
 }
 
 //post one
 async function addRecipe(recipe) {
-    return await axios.post('/api/recipes', recipe);
+    return await axios.post(apiserver + '/api/recipes', recipe);
 }
 
 //patch one, ei vielä käytössä
 async function updateRecipe(recipe) {
-    return await axios.patch(`/api/recipes/${recipe.id}`, recipe)
+    return await axios.patch(apiserver + `/api/recipes/${recipe.id}`, recipe);
 }
 
 //delete one
 async function deleteRecipe(id) {
-    return await axios.delete(`/api/recipes/${id}`)
+    return await axios.delete(apiserver + `/api/recipes/${id}`);
 }
 
 
